df3ed582908a88004fe4ed385df42281e943fb24
Olena K <234inbox@gmail.com>
2023-02-01 10:52:49 -0500

    C2 Creation de modèle et de boucle

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
-<?php get_header() ?> 
-<h1>Bonjour tout le monde, bienvenue au cours 31w</h1> 
+<?php 
+/**
+    Modele index.php represente le modele par defaut du theme
+*/ 
+?>
+<?php get_header()?> 
+<main>
+    <h3>index.php</h3>
+    <?php 
+        if (have_posts()):
+            while (have_posts()) : the_post();
+            the_title('<h1>','</h1>');
+        endwhile;
+        endif;
+    ?> 
+</main>   
+<?php get_footer(); ?>
