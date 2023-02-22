f364c1bb21888340135361539c63c467b2cb8b58
Olena K <234inbox@gmail.com>
2023-02-22 11:05:00 -0500

    C7 Modifications des paramètres de fonction cidweb_modifie_requete_principal() et ajout des commentaires

--- functions.php
+++ functions.php
@@ -40,12 +40,14 @@ add_theme_support( 'custom-logo',
  * Dans ce cas çi nous filtrons la requête de la page d'accueil
  * @param WP_query  $query la requête principal de WP
 */
-function cidweb_modifie_requete_principal( $query ) {
-    if ( $query->is_home() && $query->is_main_query() && ! is_admin() ) {
-      $query->set( 'category_name', 'note4w4' );
-      $query->set( 'orderby', 'title' );
-      $query->set( 'order', 'ASC' );
-      }
-     }
-     add_action( 'pre_get_posts', 'cidweb_modifie_requete_principal' );
+    function cidweb_modifie_requete_principal( $query ) {
+        if ( $query->is_home() // si page d'accueil
+            && $query->is_main_query()// si raquete principale
+            && ! is_admin() ) { // non tableu de bord
+            $query->set( 'category_name', 'note-wp' ); // filtre les articles de categorie "note-wp"
+            $query->set( 'orderby', 'title' ); // trie selon le titre
+            $query->set( 'order', 'ASC' ); // ordre ascendant
+        }
+    }
+     add_action( 'pre_get_posts', 'cidweb_modifie_requete_principal' ); // = event listener
                    
