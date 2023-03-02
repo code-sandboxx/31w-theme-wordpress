98cbeb4809846912ec89db9907c520f9867395d8
Olena K <234inbox@gmail.com>
2023-03-02 16:36:42 -0500

    C10 Ajout de galerie d'images

--- front-page.php
+++ front-page.php
@@ -9,8 +9,14 @@ get_header() ?>
     <section class="blocflex">            
         <?php 
         if (have_posts()):
-            while (have_posts()) : the_post();               
-                 get_template_part("template-parts/categorie", "note-wp"); ?>
+            while (have_posts()) : the_post();   
+            if(in_category('galerie')){
+                get_template_part("template-parts/categorie", "galerie");
+            }       
+            else{
+                get_template_part("template-parts/categorie", "note-wp");
+            }     
+        ?>
             <?php endwhile;?>
         <?php endif; ?> 
     </section>
---
+++ template-parts/categorie-galerie.php
@@ -0,0 +1,12 @@
+<?php
+/*
+* template-part qui permettra d'afficher un article provenant d'un conteneur de *classe blocflex
+*pour un article de catégorie galerie
+*/
+
+?>
+<article class="blocflex__galerie">
+      
+   <?php the_content(); ?>
+
+</article>
