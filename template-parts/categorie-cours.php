fdb8b92330708f4d20b8e7d45e57389d8a29c6be
Olena K <234inbox@gmail.com>
2023-03-26 18:49:12 -0400

    C16 Refonte de la palitre des couleurs et creation des variables

--- template-parts/categorie-cours.php
+++ template-parts/categorie-cours.php
@@ -11,6 +11,9 @@ $duree = substr($titre, -4, 3); // a compléter
 <article class = "blocflex__article">
     <h5><a href="<?php the_permalink(); ?>"> <?= $sigle; ?></a></h5>   
     <p><?= wp_trim_words(get_the_excerpt(), 15) ?></p>
-    <p><?php the_field('enseignant'); // normal si soulignée - la fonction se trouve pas dans notre WP ?> </p> 
+    <div class="acf">
+        <p class="champ__enseignant"><?php the_field('enseignant'); // normal si soulignée - la fonction se trouve pas dans notre WP ?> </p> 
+        <p class="champ__domaine"><?php the_field('domaine'); ?></p>
+    </div>    
     <p class="duree"><?= $duree; ?></p>
 </article>
